c1d81340b2112f4d15d11f632b1d4059f1be6db9
Jonathan C MacMillan <jmacmill@gmail.com>
2023-04-23 17:24:19 +1000

    create redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
