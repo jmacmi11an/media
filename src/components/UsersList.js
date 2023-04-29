6fda6d830f82b112f56577b0427b3e0bb2164012
Jonathan C MacMillan <jmacmill@gmail.com>
2023-04-29 11:50:29 +1000

    Create custom thunk hook

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,30 +1,14 @@
-import { useEffect, useState, useCallback } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from "react";
+import { useSelector } from 'react-redux';
 import { fetchUsers, addUser } from "../store";
+import { useThunk } from "../hooks/use-thunk";
 import Button from './Button';
 import Skeleton from "./Skeleton";
 
-function useThunk(thunk) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const dispatch = useDispatch();
-
-    const runThunk = useCallback(() => {
-        setIsLoading(true)
-        dispatch(thunk())
-            .unwrap()
-            .catch((err) => setError(err))
-            .finally(() => setIsLoading(false));
-    }, [dispatch, thunk]);
-
-    return [runThunk, isLoading, error];
-}
 
 function UsersList () {
     const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
-    const [isCreatingUser, setIsCreatingUser] = useState(false);
-    const [creatingUserError, setCreatingUserError] = useState(null);
-    const dispatch = useDispatch();
+    const [doCreateUser, isCreatingUser, creatingUserError] = useThunk(addUser)
     const { data } = useSelector((state) => {
         return state.users;
     })
@@ -34,11 +18,7 @@ function UsersList () {
     }, [doFetchUsers]);
 
     const handleUserAdd = () => {
-        setIsCreatingUser(true)
-        dispatch(addUser())
-            .unwrap()
-            .catch((err) => setCreatingUserError(err))
-            .finally(() => setIsCreatingUser(false));
+        doCreateUser();
     };
 
     if (isLoadingUsers) {
