8f75dd6bd10dd8c51721ac638f583dd6f5519315
Jonathan C MacMillan <jmacmill@gmail.com>
2023-05-01 16:22:14 +1000

    Connect API to the redux store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { albumsApi } from "./apis/albumsApi";
 import { usersReducer } from './slices/usersSlice';
 
 export const store = configureStore({
     reducer: {
         users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
     },
+    middleware: (getDefaultMiddleware) => {
+        return getDefaultMiddleware()
+            .concat(albumsApi.middleware);
+    }
 });
+
+setupListeners(store.dispatch);
 
 export * from './thunks/fetchUsers';
 export * from './thunks/addUser';
